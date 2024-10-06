9521a8d6053f4bb13545da5b464626236b415da0
71cj34 <84353892+71cj34@users.noreply.github.com>
2024-10-05 21:23:29 -0400

    Initial commit

---
+++ countparameters_dev.py
@@ -0,0 +1,64 @@
+from safetensors import safe_open
+import os
+import yaml
+
+with open('settings.yaml', 'r') as f:
+    data = yaml.load(f, Loader=yaml.SafeLoader)
+llamaPath = data['outputPath']
+
+total_params = 0
+manualcheck = 0
+list_params = []
+rounded_params = 0
+unrounded_params = 0
+
+print("\n\n\n BOOTED ParameterCount DEV MODE.\n\n")
+decimals = int(input('How many decimals to round to: \n\n'))
+
+def count_parameters_in_safetensors(file_path: str) -> int:
+    total_params = 0
+    with safe_open(file_path, framework="pt") as f:
+        for key in f.keys():
+            tensor = f.get_tensor(key)
+            total_params += tensor.numel()
+    return total_params
+
+def find_all(path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(".safetensors"):
+                result.append(root + "/" + file)
+    return result
+
+paths = find_all(llamaPath)
+
+for i in range(len(paths)):
+    midcount = count_parameters_in_safetensors(paths[i])
+    total_params += midcount
+    i += 1
+
+total_params = total_params / 1000000000
+inter_params = str(total_params)
+list_params = list(inter_params)
+unrounded_params = round(total_params, decimals)
+
+
+if list_params[0] in [3, 7, 8, 9]:
+    del list_params[1:]
+elif list_params[0:2] in [13, 20, 33, 34, 70]:
+    del list_params[2:]
+elif list_params[0:3] in [101, 120]:
+    del list_params[3:]
+else:
+    rounded_params = round(total_params, 2)
+    manualcheck = 1
+
+if(manualcheck == 0):
+    concatenated_string = ''.join(map(str, list_params))
+    total_params = float(concatenated_string)
+    print("Automatic rounding: " + str(total_params) + "b\n")
+else:
+    print("Automatic rounding unsuccessful.\n")
+print("Rounded (2): " + str(rounded_params) + "b\n")
+print("Rounded (" + str(decimals) + "): " + str(unrounded_params) + "b\n")
---
+++ merge.bat
@@ -0,0 +1,74 @@
+@echo off
+
+set YAML_FILE=settings.yaml
+
+for /f "tokens=2 delims== " %%a in ('yq e ".condaPath" %YAML_FILE%') do set CONDA_PATH=%%a
+for /f "tokens=2 delims== " %%a in ('yq e ".mergekitConfigPath" %YAML_FILE%') do set CONFIG_PATH=%%a
+for /f "tokens=2 delims== " %%a in ('yq e ".outputPath" %YAML_FILE%') do set OUTPUT_PATH=%%a
+for /f "tokens=2 delims== " %%a in ('yq e ".llamacppBinPath" %YAML_FILE%') do set LLAMA_BINPATH=%%a
+for /f "tokens=2 delims== " %%a in ('yq e ".llamacppPath" %YAML_FILE%') do set LLAMA_PATH=%%a
+for /f "tokens=2 delims== " %%a in ('yq e ".useCUDA" %YAML_FILE%') do set CUDA=%%a
+
+set /p SimpleMode=Do you want to run in Simple Mode (only create Q4_K_M)? (y/n): 
+
+set /p Crime=Allow crimes? (y/n) 
+
+set /p ModelName=Enter the name for your model: 
+
+set /p ParamCount=Enter the Number of Expected Parameters (blank for auto): 
+
+if "%Crime%"=="y" set "allowcrime=--allow-crimes"
+if "%CUDA%"=="true" set "cuda=--cuda"
+
+mergekit-yaml %CONFIG_PATH% %OUTPUT_PATH%\F16 %cuda% --write-model-card %allowcrime%
+
+if "%ParamCount%"=="" (
+   echo Activating Conda...
+   call "%CONDA_PATH%\Scripts\activate.bat" "%CONDA_PATH%"
+   echo Conda Activated!
+
+   python countparameters.py
+
+   for /f "delims=" %%i in ('python countparameters.py') do set ParamCount=%%i
+
+   call conda deactivate
+
+)
+
+echo Estimated paramcount is: %ParamCount%
+pause
+
+cd %LLAMA_PATH%
+
+py %LLAMA_PATH%\convert_hf_to_gguf.py %OUTPUT_PATH%
+
+for /R "%OUTPUT_PATH%" %%F in (*F16*) do (
+    set "A=%%F"
+    goto :found
+)
+
+:found
+
+if not defined A (
+    echo No file with "F16" in its name was found.
+    pause
+    exit /b 1
+)
+
+if /I "%SimpleMode%"=="y" (
+    %LLAMA_BINPATH%\llama-quantize.exe "%A%" %OUTPUT_PATH%\%ModelName%-%ParamCount%.Q4_K_M.gguf Q4_K_M
+) else (
+    %LLAMA_BINPATH%\llama-quantize.exe "%A%" %OUTPUT_PATH%\%ModelName%-%ParamCount%.Q2_K.gguf Q2_K
+    %LLAMA_BINPATH%\llama-quantize.exe "%A%" %OUTPUT_PATH%\%ModelName%-%ParamCount%.Q3_K_S.gguf Q3_K_S
+    %LLAMA_BINPATH%\llama-quantize.exe "%A%" %OUTPUT_PATH%\%ModelName%-%ParamCount%.Q3_K_M.gguf Q3_K_M
+    %LLAMA_BINPATH%\llama-quantize.exe "%A%" %OUTPUT_PATH%\%ModelName%-%ParamCount%.Q3_K_L.gguf Q3_K_L
+    %LLAMA_BINPATH%\llama-quantize.exe "%A%" %OUTPUT_PATH%\%ModelName%-%ParamCount%.Q4_K_S.gguf Q4_K_S
+    %LLAMA_BINPATH%\llama-quantize.exe "%A%" %OUTPUT_PATH%\%ModelName%-%ParamCount%.Q4_K_M.gguf Q4_K_M
+    %LLAMA_BINPATH%\llama-quantize.exe "%A%" %OUTPUT_PATH%\%ModelName%-%ParamCount%.Q5_K_S.gguf Q5_K_S
+    %LLAMA_BINPATH%\llama-quantize.exe "%A%" %OUTPUT_PATH%\%ModelName%-%ParamCount%.Q5_K_M.gguf Q5_K_M
+    %LLAMA_BINPATH%\llama-quantize.exe "%A%" %OUTPUT_PATH%\%ModelName%-%ParamCount%.Q6_K.gguf Q6_K
+    %LLAMA_BINPATH%\llama-quantize.exe "%A%" %OUTPUT_PATH%\%ModelName%-%ParamCount%.Q8_0.gguf Q8_0
+)
+
+echo Script completed successfully.
+pause
